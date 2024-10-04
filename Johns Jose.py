5392e1217af29c23f08be6353fcd5a0d6297510c
Johns Jose <jjkanichukattu@gmail.com>
2024-10-04 16:01:59 +0530

    My first program in python

---
+++ Johns Jose.py
@@ -0,0 +1,81 @@
+#print("Hello,World")
+#print("Hello,\nWorld")
+
+'''
+if 5 > 2:
+    print("Five is Greater than Two")
+
+
+
+if 5 > 2:
+    print("Five is Greater than Two")
+if 5 > 2:
+      print("Five is Greater than Two")
+if 5 > 2:
+            print("Five is Greater than Two")
+
+
+if 5 > 2:
+    print("Five is Greater than Two")
+    print("Five is Greater than Two")
+
+x = 2
+x = "Johns"
+print(x)
+
+
+x = str(3)
+y = int(3)
+z = float(3.2)
+
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "Johns"
+z = 3.2
+a = 3j
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+
+myvar ="Johns"
+my_var ="Johns"
+_my_var ="Johns"
+myVar ="Johns"
+MYVAR ="Johns"
+
+x, y, z = "Orange", "Apple", "Pineapple"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+username = input("Enter username:")
+print("Username is: " + username)
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+'''
+
+a = "Hello, World"
+print(a[6])
+
+b = "Hello, World"
+print(b[:5])
+
+
