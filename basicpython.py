b6d41463303f5c3291d886e3f626507aebfe1fcf
Johns Jose <jjkanichukattu@gmail.com>
2024-10-18 14:34:57 +0530

    Add files via upload

---
+++ basicpython.py
@@ -0,0 +1,13 @@
+'''
+Authore: Johns Jose
+Date: 18-10-2024
+Description: program that asks the user to input their name and age.
+After receiving the input,
+display a message that includes the user's name and age.
+Hello, John! You are 25 years old.
+'''
+
+name=input("Enter Your Name:")
+age=input("Enter your age:")
+print("Hello,",name+"! you are",age,"years old")
+
