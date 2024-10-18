2dcb2124e07de60f515e3d48ec587bb3c7dcd498
Johns Jose <jjkanichukattu@gmail.com>
2024-10-18 16:23:22 +0530

    Add files via upload

--- Python_MATH_Module.py
+++ Python_MATH_Module.py
@@ -1,3 +1,13 @@
+'''
+Authore: Johns Jose
+Date: 18-10-2024
+Description: Write a Python program that uses functions from the math module to perform the following operations on a number provided by the user:
+    Find the square root.
+    Calculate the factorial.
+    Raise the number to the power of 2.
+'''
+
+
 import math
 number=int(input("Enter a number:"))
 Square_root=math.sqrt(number)
