a004f817db87f40ee46d8a1dc4119e5dcc586e22
Johns Jose <jjkanichukattu@gmail.com>
2024-10-18 16:18:53 +0530

    Add files via upload

---
+++ Python_MATH_Module.py
@@ -0,0 +1,8 @@
+import math
+number=int(input("Enter a number:"))
+Square_root=math.sqrt(number)
+print("Square root of ",number,":",Square_root)
+Factorial=math.factorial(number)
+print("Factorial of ",number,":",Factorial)
+Power=math.pow(number,2)
+print(number,"raised to power 2",Power)
