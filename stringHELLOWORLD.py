c23f9b9cbe41c84289a6cff2ae70381bda7d08e7
Johns Jose <jjkanichukattu@gmail.com>
2024-10-18 15:07:17 +0530

    Add files via upload

---
+++ stringHELLOWORLD.py
@@ -0,0 +1,16 @@
+'''
+Authore: Johns Jose
+Date: 18-10-2024
+Description: A Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+Instructions:
+    Assign the string "Hello, World!" to a variable.
+    Extract the substring "World" from the string.
+    Concatenate this substring with the string " Everyone!".
+    Print the final concatenated string.
+'''
+
+str_VAR_1 = "Hello, World!"
+str_VAR_2 = str_VAR_1[7:12]
+print(str_VAR_2)
+str_VAR_3 = " Everyone!"
+print(str_VAR_2+str_VAR_3)
