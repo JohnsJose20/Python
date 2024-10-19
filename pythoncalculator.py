0324aa93d57bc4a4fa540967c03e6d708c458073
Johns Jose <jjkanichukattu@gmail.com>
2024-10-19 14:45:44 +0530

    Add files via upload

---
+++ pythoncalculator.py
@@ -0,0 +1,20 @@
+'''Authore: Johns Jose
+Date: 19-10-2024
+a Python program that performs the following tasks:
+User Input,Addition,Sum,etc.'''
+
+num1=int(input("Enter the num1: "))
+num2=int(input("Enter the num2: "))
+num3=int(input("Enter the num3: "))
+Sum=(num1+num2)
+print("The sum of num1 and num2 is: ",Sum)
+Difference=(num2-num1)
+print("The The difference when num2 is subtracted from num1 is: ",Difference)
+Product=(num1*num2)
+print("The product of num1 and num2 is: ",Product)
+Division=(num1/num2)
+print("The quotient when num1 is divided by num2 is: ",Division)
+Modulus=(num1%num2)
+print("The remainder when num1 is divided by num2 is: ",Modulus)
+result=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2 is: ",result)
