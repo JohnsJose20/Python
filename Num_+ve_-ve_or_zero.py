ad8efd471179fc4e9bff98c48d8ba24108ca0eb0
Johns Jose <jjkanichukattu@gmail.com>
2024-10-25 14:36:53 +0530

    Add files via upload

---
+++ Num_+ve_-ve_or_zero.py
@@ -0,0 +1,13 @@
+'''
+Authore: Johns Jose
+Date: 25-10-2024
+Description: Write a Python program to check the given number is q positive,negative or zero
+'''
+
+Num_1=int(input("Enter first number:"))
+if(Num_1>0):
+    print(Num_1," is positive")
+elif(Num_1<0):
+    print(Num_1," is negative")
+else:
+    print(Num_1," is a zero")
