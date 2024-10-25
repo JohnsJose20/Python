6e4217b41c237e58c3bffc5a7b17f27fdeaec92e
Johns Jose <jjkanichukattu@gmail.com>
2024-10-25 14:29:08 +0530

    Largest_number.py

--- Largest_number.py
+++ Largest_number.py
@@ -1,3 +1,9 @@
+'''
+Authore: Johns Jose
+Date: 25-10-2024
+Description: Write a Python program to find the largest of two numbers
+'''
+
 Num_1=int(input("Enter first number:"))
 Num_2=int(input("Enter second number:"))
 if(Num_1>Num_2):
