94502d980c6036bdb8ddaf802e0e98b78ba8ff21
Johns Jose <jjkanichukattu@gmail.com>
2024-10-25 14:20:20 +0530

    Add files via upload

---
+++ Largest_number.py
@@ -0,0 +1,8 @@
+Num_1=int(input("Enter first number:"))
+Num_2=int(input("Enter second number:"))
+if(Num_1>Num_2):
+    print(Num_1,"is greater than ",Num_2)
+else:
+    print(Num_1,"is less than ",Num_2)
+
+
