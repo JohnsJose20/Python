53cefc2a44661cba5d6bc7607d84a15eee52ca9a
Johns Jose <jjkanichukattu@gmail.com>
2024-10-25 14:32:28 +0530

    Add files via upload

---
+++ Number_positive_or_not.py
@@ -0,0 +1,11 @@
+'''
+Authore: Johns Jose
+Date: 25-10-2024
+Description: Write a Python program to check the given number is positive or not
+'''
+
+Num_1=int(input("Enter first number:"))
+if(Num_1>0):
+    print(Num_1,"is positive")
+else:
+    print(Num_1,"is negative")
